7303580c726381d02b7d078dec556c77f555f7b1
martinwoo7 <martinwooo@hotmail.com>
2023-08-21 20:44:47 -0700

    initial django backend commit

---
+++ backend/todo/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    # specifies the model to work with and the fields to be converted to JSON
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
